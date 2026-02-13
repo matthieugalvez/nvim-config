8553c5e30f4471f3f8d1fadd39cf440bbbd19c33
Matthieu GALVEZ <mgalvez@student.42.fr>
2026-02-13 12:02:52 +0100

    impl treesitter main branch

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -3,9 +3,8 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local treesitter = require("nvim-treesitter")
-
-		treesitter.install({
+		local ts = require("nvim-treesitter")
+		local parsers = {
 			"asm",
 			"bash",
 			"c",
@@ -24,6 +23,30 @@ return {
 			"toml",
 			"yaml",
 			"zig",
+		}
+
+		for _, parser in ipairs(parsers) do
+			ts.install(parser)
+		end
+
+		local patterns = {}
+
+		for _, parser in ipairs(parsers) do
+			local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+			for _, pp in pairs(parser_patterns) do
+				table.insert(patterns, pp)
+			end
+		end
+
+		vim.treesitter.language.register("groovy", "Jenkinsfile")
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = patterns,
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
 	end,
 }
