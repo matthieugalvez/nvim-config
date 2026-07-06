45419cea2ea1d441a4c232dadbf03dabd0afbd62
Matthieu GALVEZ <mgalvez@student.42.fr>
2026-07-06 16:09:17 +0200

    biome html integration

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,6 +20,34 @@ return {
 		local config_path = vim.fn.stdpath("config") .. "/config"
 
 		return {
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+
+			formatters_by_ft = {
+				bash = { "shellcheck", "shfmt" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				css = { "biome" },
+				dockerfile = { "dockerfmt" },
+				html = { "biome" },
+				javascript = { "biome" },
+				javascriptreact = { "biome" },
+				json = { "biome" },
+				jsonc = { "biome" },
+				lua = { "stylua" },
+				markdown = { "biome" },
+				python = { "ruff_fix", "ruff_format" },
+				rust = { "rustfmt" },
+				scss = { "biome" },
+				toml = { "taplo" },
+				typescript = { "biome" },
+				typescriptreact = { "biome" },
+				yaml = { "biome" },
+			},
+
 			formatters = {
 				ruff_format = {
 					args = {
@@ -33,34 +61,17 @@ return {
 						"--style=file:" .. config_path .. "/clang-format/.clang-format",
 					},
 				},
-			},
 
-			formatters_by_ft = {
-				bash = { "shellcheck", "shfmt" },
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				css = { "biome" },
-				dockerfile = { "dockerfmt" },
-				-- html = { "biome" },
-				javascript = { "biome" },
-				javascriptreact = { "biome" },
-				json = { "biome" },
-				jsonc = { "biome" },
-				lua = { "stylua" },
-				-- markdown = { "biome" },
-				python = { "ruff_fix", "ruff_format" },
-				rust = { "rustfmt" },
-				scss = { "biome" },
-				toml = { "taplo" },
-				typescript = { "biome" },
-				typescriptreact = { "biome" },
-				yaml = { "biome" },
-			},
-
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
+				biome = {
+					command = "biome",
+					args = {
+						"check",
+						"--write",
+						"--stdin-file-path",
+						"$FILENAME",
+						"--config-path=" .. config_path .. "/biome/biome.json",
+					},
+				},
 			},
 		}
 	end,
