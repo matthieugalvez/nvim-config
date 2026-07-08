cfb03d6b484b2a4b80d80b418af3b42a186b1c05
mgalvez <mgalvez@student.42.fr>
2026-07-08 04:20:43 +0200

    simpler lualine config

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -4,35 +4,21 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 
-	opts = function()
-		local lazy_status = require("lazy.status")
+	opts = {
+		options = {
+			globalstatus = true,
+		},
 
-		return {
-			options = {
-				globalstatus = true,
+		sections = {
+			lualine_c = {
+				{ "filename", path = 1 },
 			},
+		},
 
-			sections = {
-				lualine_c = {
-					{ "filename", path = 1 },
-				},
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = "DiagnosticWarn",
-					},
-					"encoding",
-					"fileformat",
-					"filetype",
-				},
-			},
-
-			extensions = {
-				"neo-tree",
-				"mason",
-				"lazy",
-			},
-		}
-	end,
+		extensions = {
+			"lazy",
+			"mason",
+			"neo-tree",
+		},
+	},
 }
